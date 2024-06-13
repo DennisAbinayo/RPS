235b1ec40bf08634eb0b7d44ac9c982956a698e0
DennisAbinayo <dennisabinayo5@gmail.com>
2024-06-13 22:16:01 +0300

    rock paper scissors

---
+++ sandbox.js
@@ -0,0 +1,59 @@
+// Declare a variable to store the array of choices (rack,paper and scissors)
+let choices = ['rock', 'paper', 'scissors'];
+
+
+// Implementing a score counter
+let playerScore = 0;
+let computerScore = 0;
+let tieScore = 0;
+
+// Targetting the button with id of rock to add event listener
+let rock = document.getElementById('rock');
+rock.addEventListener('click', () => {
+playRound('rock');
+});
+
+
+// Targetting the button with id of paper to add event listener
+let paper = document.getElementById('paper');
+paper.addEventListener('click', () => {
+playRound('paper');
+});
+
+// Targetting the button with id of scissors to add event listener
+let scissors = document.getElementById('scissors');
+scissors.addEventListener('click', () => {
+playRound('scissors');
+});
+
+
+// Adding function
+function playRound(playerChoice) {
+let computerChoice = choices[Math.floor(Math.random() * choices.length)]; // generating a random computer choice
+let result = '';
+
+// Using if statement to do comparison
+if (playerChoice === computerChoice) {
+    result = "It's a tie";
+    tieScore++;
+} else if ((playerChoice === 'rock' && computerChoice === 'scissors') ||
+            (playerChoice === 'paper' && computerChoice === 'rock') ||
+            (playerChoice === 'scissors' && computerChoice === 'paper')) {
+    result = 'You win';
+    playerScore++;
+} else {
+    result = 'You lose';
+    computerScore++;
+}
+
+// Adding outcome to the div with result id
+let outcome = document.getElementById('result');
+outcome.innerText = `${result} \n (Player: ${playerScore}, Computer: ${computerScore})`;
+}
+
+
+//Resetting 
+let resetBtn = document.getElementById('reset').addEventListener('click',()=>{
+    let outcome = document.getElementById('result');
+    outcome.innerText = '';
+});

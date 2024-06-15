f89282dc34bf24cd66ba81b18c1587dfaf5413d4
DennisAbinayo <dennisabinayo5@gmail.com>
2024-06-15 10:43:14 +0300

    updated js

--- sandbox.js
+++ sandbox.js
@@ -1,7 +1,3 @@
-// Declare a variable to store the array of choices (rock,paper and scissors)
-let choices = ['rock', 'paper', 'scissors'];
-
-
 // Implementing a score counter
 let playerScore = 0;
 let computerScore = 0;
@@ -10,45 +6,47 @@ let tieScore = 0;
 // Targetting the button with id of rock, to add event listener
 let rock = document.getElementById('rock');
 rock.addEventListener('click', () => {
-playRound('rock');
+    playRound('rock');
 });
 
 
 // Targetting the button with id of paper to add event listener
 let paper = document.getElementById('paper');
 paper.addEventListener('click', () => {
-playRound('paper');
+    playRound('paper');
 });
 
 // Targetting the button with id of scissors to add event listener
 let scissors = document.getElementById('scissors');
 scissors.addEventListener('click', () => {
-playRound('scissors');
+    playRound('scissors');
 });
 
 
 // Adding function
 function playRound(playerChoice) {
-let computerChoice = choices[Math.floor(Math.random() * choices.length)]; // generating a random computer choice
-let result = '';
+    let choices = ['rock', 'paper', 'scissors'];
 
-// Using if statement to do comparison
-if (playerChoice === computerChoice) {
-    result = "It's a tie";
-    tieScore++;
-} else if ((playerChoice === 'rock' && computerChoice === 'scissors') ||
-            (playerChoice === 'paper' && computerChoice === 'rock') ||
-            (playerChoice === 'scissors' && computerChoice === 'paper')) {
-    result = 'You win';
-    playerScore++;
-} else {
-    result = 'You lose';
-    computerScore++;
-}
+    let computerChoice = choices[Math.floor(Math.random() * choices.length)]; // generating a random computer choice
+    let result = '';
 
-// Adding outcome to the div with result id
-let outcome = document.getElementById('result');
-outcome.innerText = `${result} \n (Player: ${playerScore}, Computer: ${computerScore}, Tie: ${tieScore})`;
+    // Using if statement to do comparison
+    if (playerChoice === computerChoice) {
+        result = "It's a tie";
+        tieScore++;
+    } else if ((playerChoice === 'rock' && computerChoice === 'scissors') ||
+                (playerChoice === 'paper' && computerChoice === 'rock') ||
+                (playerChoice === 'scissors' && computerChoice === 'paper')) {
+        result = 'You win';
+        playerScore++;
+    } else {
+        result = 'You lose';
+        computerScore++;
+    }
+
+    // Adding outcome to the div with result id
+    let outcome = document.getElementById('result');
+    outcome.innerText = `${result} \n (Player: ${playerScore}, Computer: ${computerScore}, Tie: ${tieScore})`;
 }
 
 
